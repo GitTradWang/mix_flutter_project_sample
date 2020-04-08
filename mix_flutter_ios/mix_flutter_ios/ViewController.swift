075454bd81ccc88d8f8fd8d04c4c6dd3d751b520
tradwang <wangxiaojun@bianla.cn>
2020-04-08 10:42:41 +0800

    feat(init): init ios

---
+++ mix_flutter_ios/mix_flutter_ios/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  mix_flutter_ios
+//
+//  Created by TradWang on 2020/4/8.
+//  Copyright © 2020 TradWang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
